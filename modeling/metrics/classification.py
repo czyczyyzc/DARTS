e3eed88413ea5f25ba79d558944ae0cc3c869988
czyczyyzc <czyczyyzc@126.com>
2023-05-20 13:07:45 +1000

    fix a bug

--- modeling/metrics/classification.py
+++ modeling/metrics/classification.py
@@ -13,6 +13,6 @@ def accuracy(output, target, topk=(1,)):
 
     ret = []
     for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
+        correct_k = correct[:k].contiguous().view(-1).float().sum(0)
         ret.append(correct_k.mul_(100.0 / batch_size))
     return ret
